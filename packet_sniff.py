82c2ac8c495a57a65c5ae84b90d142f085173345
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2023-11-21 17:55:54 +0530

    added demo folder

--- packet_sniff.py
+++ packet_sniff.py
@@ -4,8 +4,8 @@ def packet_callback(packet):
     print(packet.summary())
     
 def main():
-    sniff(iface = 'eno1', prn=packet_callback, filter='tcp')
-    # sniff(iface = 'wlo1', prn=packet_callback, filter='tcp')
+#    sniff(iface = 'eno1', prn=packet_callback, filter='tcp')
+     sniff(iface = 'wlo1', prn=packet_callback, filter='tcp')
     
 if __name__ == '__main__':
     main()
--- syn-filter.py
+++ syn-filter.py
@@ -1,7 +1,7 @@
 file_path = '/home/siddhant/Documents/project_ccn/cap.txt'
 
 with open(file_path, 'r') as file:
-    new_lines = [line for line in file if line.rstrip().endswith('S')]
+    new_lines = [line for line in file if line.rstrip().endswith('A')]
 
 # Now new_lines contains the lines that end with 'S'
 for i in new_lines:
